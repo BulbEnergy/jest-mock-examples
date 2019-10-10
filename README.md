cc53e1ccb566a3253ad402dd998357ac0377134a
Amina Layco <amina@bulb.co.uk>
2019-10-10 10:31:09 +0100

    Add license and few improvements

---
+++ README.md
@@ -0,0 +1,15 @@
+# Jest mock examples
+
+The purpose of this repo is to list examples of different types of mocking in Jest
+so anyone can use it as a reference.
+
+It uses:
+  1. Jest
+  2. Typescript
+  3. Prettier
+
+If you have other useful examples or comments, please feel free to contribute.
+
+Install: `yarn`
+
+Run tests: `yarn test`
--- class-mock/mock-full-with-implementation.test.ts
+++ class-mock/mock-full-with-implementation.test.ts
@@ -1,16 +1,17 @@
 import { ComplexMath } from './ComplexMath';
 
-// According to docs this needs to be named like "mock..." but works anyways
-const fakeSum = jest.fn(() => 8);
+// The docs say this variable needs to be called "mock..."
+// If you get "ReferenceError: ... is not defined" you can use "var" instead of "const"
+const mockSum = jest.fn(() => 8);
 
 // Can't be arrow function if tsconfig target > es5: BasicMath_1.BasicMath is not a constructor
 // jest.mock('./BasicMath', () => ({
-//   BasicMath: () => ({ sum: fakeSum }),
+//   BasicMath: () => ({ sum: mockSum }),
 // }));
 
 jest.mock('./BasicMath', () => ({
   BasicMath: function() {
-    return { sum: fakeSum };
+    return { sum: mockSum };
   },
 }));
 
@@ -20,6 +21,6 @@ describe('ComplexMath:class full mock with implementation', () => {
 
     expect(complexMath.avg(1, 1)).toEqual(4); // sum is mocked
 
-    expect(fakeSum).toBeCalledTimes(1);
+    expect(mockSum).toBeCalledTimes(1);
   });
 });
