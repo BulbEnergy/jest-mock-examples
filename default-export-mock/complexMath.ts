9d2b4f41bb8ad1862e9cba54238a6bfebf7ef2ec
Amina Layco <amina@bulb.co.uk>
2019-10-16 13:44:50 +0100

    Add default export example

---
+++ default-export-mock/complexMath.ts
@@ -0,0 +1,5 @@
+import sum from './basicMath';
+
+export const avg = (a: number, b: number): number => {
+  return sum(a, b) / 2;
+};
--- default-export-mock/mock-with-default.test.ts
+++ default-export-mock/mock-with-default.test.ts
@@ -1,35 +1,30 @@
 import * as complexMath from './complexMath';
-import * as basicMath from './basicMath';
+import sum from './basicMath';
 
-// jest.mock is the first thing that runs in this file (also before the imports)
 jest.mock('./basicMath'); // mocks all functions in basicMath
 
-const basicMathMock = basicMath as jest.Mocked<typeof basicMath>;
+const sumMock = sum as jest.MockedFunction<typeof sum>;
 
 describe('complexMath:module full mock', () => {
   beforeEach(() => {
-    // not necessary because of jest.Mocked
-    // (basicMath.sum as jest.Mock).mockReset();
-    basicMathMock.sum.mockReset();
+    sumMock.mockReset();
   });
 
   test('mock sum without return value', () => {
     const result = complexMath.avg(4, 2);
 
-    expect(basicMath.sum).toBeCalledWith(4, 2);
-
-    expect(basicMath.divide).not.toBeCalledWith(4, 2);
+    expect(sumMock).toBeCalledWith(4, 2);
 
     // because sum is mocked but not implemented
     expect(result).toBe(NaN);
   });
 
   test('mock sum with return value', () => {
-    basicMathMock.sum.mockReturnValue(8);
+    sumMock.mockReturnValue(8);
 
     const result = complexMath.avg(4, 2);
 
-    expect(basicMath.sum).toBeCalledWith(4, 2);
+    expect(sumMock).toBeCalledWith(4, 2);
 
     // sum returns 8, it doesn't matter we pass 4 and 2
     expect(result).toBe(4);
